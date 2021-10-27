5d9f61cb105ebeaface5611c7181c77b891db49a
Carlo <cdiaz014@odu.edu>
2021-10-27 16:56:26 -0400

    Skeleton code

---
+++ CommandLineProcessor.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.ArrayList;
+
+//Processes the command line parameters
+
+public class CommandLineProcessor {
+
+    private int numRefactors;
+    private ArrayList<File> fileList = new ArrayList<File>();
+
+    //Finds files with C++ extensions
+    public void findCppFiles(){
+
+    }
+
+    //Finds files that were inputted in the CLI by the user
+    public void findInputFiles(){
+
+    }
+
+    //numRefactors as an argument
+    public static void main(String[] args){
+
+    }
+}
