8b23b0ee1a8366c7a3091b05a47fb4810eaf2ee5
Vaelyn Dulce <vdulc001@odu.edu>
2021-10-29 00:07:48 -0400

    setup android folder conventions

--- src/main/java/edu/odu/cs/cs350/HelloWorld.java
+++ src/main/java/edu/odu/cs/cs350/HelloWorld.java
@@ -1,11 +1,10 @@
 package edu.odu.cs.cs350;
 
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 
 	}
-
+	
 }
---
+++ src/test/java/edu/odu/cs/cs350/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package edu.odu.cs.cs350;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
