c250f1cc4d3514b44e710cd097be16f4e379b2c4
Jacinda Rosario <jrosa005@odu.edu>
2021-10-31 16:44:50 -0400

    token tests

--- src/test/java/edu/odu/cs/cs350/TestToken.java
+++ src/test/java/edu/odu/cs/cs350/TestToken.java
@@ -4,6 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TestToken {
+
+class TestToken() {
 	
+	/*need to implement token stream and jflex scanner in order to properly test
+	*/
+	@Test
+	void testToken() {
+		Token t = new Token("123", TokenTypes.INTEGER_LITERAL, 1, 1);
+		assertEquals (TokenTypes.INTEGER_LITERAL, t.getTokenType());
+		assertEquals ("123", t.getLexeme());
+		assertEquals (1, t.getLine());
+		assertEquals (1, t.getColumn());
+	}
 }
