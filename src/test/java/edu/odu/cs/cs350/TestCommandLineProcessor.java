ae4503ea42b7d67cacbf107a3bd4fe4fc4b43e2e
cjack019@odu.edu <sTRICTLY VS@DESKTOP-HMF9D8Q>
2021-10-31 02:52:51 -0400

    Test Cases for File Exists.

--- src/test/java/edu/odu/cs/cs350/TestCommandLineProcessor.java
+++ src/test/java/edu/odu/cs/cs350/TestCommandLineProcessor.java
@@ -2,6 +2,8 @@ package edu.odu.cs.cs350;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 import javax.xml.transform.Source;
 
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,20 @@ public class TestCommandLineProcessor {
             TestCommandLineProcessor.main(new String[]{4, file1, file2, file3, file4});
         */
     }
-    
+  void testFileExists()
+  {
+	  //Edit to your Directory//
+	  File testFolder = new File("C:\Users\sTRICTLY VS\Documents\Testfolder");
+	  File testFile = new File("C:\\Users\\sTRICTLY VS\\Documents\\Testfolder\testfile.txt");
+	  //---------------------------------------------------------------------------------------//
+	  boolean Exists = testFile.exists();
+	  assertThat(testFile.exists(),is(true));
+	  assertThat(testfile.isDirectory(),is(true));
+	  assertThat(testFile.isFile(),is(false))
+	  assertThat(testFolder.exists(),is(true));
+	  assertThat(testFolder.isDirectory(),is(true));
+	  assertThat(testFolder.isFile(),is(false));
+	 	  
+  }
     
 }
