bdbee72cae69feae3dfb4286c70dd96a4220592a
Carlo Diaz <carlodiaz139@gmail.com>
2021-11-08 14:58:18 -0500

    Mutators and outputToString for Refactoring

--- src/main/java/edu/odu/cs/cs350/Refactoring.java
+++ src/main/java/edu/odu/cs/cs350/Refactoring.java
@@ -16,14 +16,39 @@ public class Refactoring {
         numRefactors = num;
     }
 
+    public void setFileList(ArrayList<SourceCodeFile> list){
+        fileList = list;
+    }
+
     public ArrayList<SourceCodeFile> getFileList(){
         return fileList;
     }
 
+    public void setNumRefactors(int num){
+        numRefactors = num;
+    }
+
+    public int getNumRefactors(){
+        return numRefactors;
+    }
+
+    public String printRefactoredtoString(){
+        String output;
+        output = "Files scanned: ";
+
+        for(int i = 0; i < fileList.size(); i++){
+            //5 is just an arbitrary number as placeholder for num refactors actually found
+            output = output + fileList.get(i).toString() + "5\n";
+        }
+
+        return output;
+    }
+
     public void printRefactored(){
-        System.out.println("Files scanned:");
-        for(int i = 0; i < numRefactors; i++){
-            System.out.println(fileList.get(0).toString() + "5");
+        System.out.println("Files scanned: ");
+        for(int i = 0; i < fileList.size(); i++){
+            //5 is just an arbitrary number as placeholder for num refactors actually found
+            System.out.println(fileList.get(i).toString() + "5");
         }
     }
 }
