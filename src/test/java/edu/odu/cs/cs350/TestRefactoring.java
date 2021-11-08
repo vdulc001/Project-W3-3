b0325d6380093d33bc017da26e4fc217263e59d0
Carlo Diaz <carlodiaz139@gmail.com>
2021-11-08 14:58:36 -0500

    Test  refactoring constructor and output

--- src/test/java/edu/odu/cs/cs350/TestRefactoring.java
+++ src/test/java/edu/odu/cs/cs350/TestRefactoring.java
@@ -6,25 +6,48 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
 import java.util.ArrayList;
 
+import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.jupiter.api.Test;
 
 public class TestRefactoring {
+
+    @Test
+    void testRefactor(){
+        int numRefactors = 10;
+        SourceCodeFile s1 = new SourceCodeFile("src/test/resources/test1.cpp");
+        SourceCodeFile s2 = new SourceCodeFile("src/test/resources/test1.h");
+        ArrayList<SourceCodeFile> list = new ArrayList<SourceCodeFile>();
+
+        Refactoring emptyRefactor = new Refactoring();
+        Refactoring refactor = new Refactoring(numRefactors, list);
+
+        assertEquals(emptyRefactor.getNumRefactors(), 0);
+        assertEquals(emptyRefactor.getFileList(), new ArrayList<SourceCodeFile>());
+
+        list.add(s1);
+
+        assertEquals(refactor.getNumRefactors(), 10);
+        assertEquals(refactor.getFileList(), list);
+
+        list.add(s2);
+        refactor.setFileList(list);
+        assertEquals(refactor.getFileList(), list);
+
+        refactor.setNumRefactors(15);
+        assertEquals(refactor.getNumRefactors(), 15);
+    }
     
     @Test
     void testOutput(){
-        SourceCodeFile s1 = new SourceCodeFile("src/test/resources/test1.txt");
-        SourceCodeFile s2 = new SourceCodeFile("src/test/resources/test2.txt");
-
+        int numRefactors = 10;
+        SourceCodeFile s1 = new SourceCodeFile("src/test/resources/test1.cpp");
+        SourceCodeFile s2 = new SourceCodeFile("src/test/resources/test1.h");
         ArrayList<SourceCodeFile> list = new ArrayList<SourceCodeFile>();
         list.add(s1);
         list.add(s2);
 
-        int numRefactors = 5;
+        Refactoring refactor = new Refactoring(numRefactors, list);
 
-        Refactoring refactor = new Refactoring(10, list);
-
-        assertNotNull(refactor.getFileList());
-
-        //assertThat(refactor.printRefactored(), is(s1.toString() + "5\n" + s2.toString() + "5\n"));
+        assertEquals(refactor.printRefactoredtoString(), "Files scanned: " + s1.toString() + "5\n" + s2.toString() + "5\n");
     }
 }
