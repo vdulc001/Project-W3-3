cf0ffe445a6b25d1bf7128d5aa172684575a5b08
cjack019@odu.edu <sTRICTLY VS@DESKTOP-HMF9D8Q.router>
2021-11-10 08:12:38 -0500

    Pull before commit

---
+++ src/main/java/edu/odu/cs/cs350/DuplicateCode.java
@@ -0,0 +1,29 @@
+package edu.odu.cs.cs350;
+import java.io.File;
+
+
+
+public class DuplicateCode{
+
+	
+private String fileoutput = "filename";
+private int line = 0;
+private int column = 0;
+
+public int getLine() { return line; }
+public int setLine(int L) { line = L; }
+
+
+public int getColumn() {return column;}
+public void setColumn(int z) {  column = z; }
+
+public String getFilename() {return fileoutput;}
+public void setFilename(String name2output) { fileoutput = name2output;}
+
+
+
+
+
+	
+	
+}
