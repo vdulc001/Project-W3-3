8807b867c0343478508d5b705b66fb6ecb671fa4
Jacinda Rosario <jrosa005@odu.edu>
2021-11-10 14:52:13 -0500

    tokenize and test the generated scanner

--- src/main/java/edu/odu/cs/cs350/SourceCodeFile.java
+++ src/main/java/edu/odu/cs/cs350/SourceCodeFile.java
@@ -1,15 +1,22 @@
 package edu.odu.cs.cs350;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 
  * @author cs_vdulc001
+ * @author cs_jrosa005
  *
  */
 public class SourceCodeFile {
 	
 	private File scf;
+	private List<Token> tokens;
 	int numberOfTokens;
 	
 	/**
@@ -46,22 +53,34 @@ public class SourceCodeFile {
 	 */
 	public int getNumberOfTokens()
 	{
+		calculateTotalTokens(tokens, numberOfTokens);
 		return numberOfTokens; 
 	}
-	
-	
+
 	/**
-	 * Calulates total number of tokens in a source code file
+	 * Calculates total number of tokens in a source code file
 	 * @return total number of tokens
 	 */
-	public int calculateTotalTokens() { return 0; };
+	public void calculateTotalTokens(final List<Token> listTok, int size) { 
+		size = listTok.size();
+	}
 	
 	/**
-	 * Converts code into sequence of tokens?
+	 * Create a list of tokens from the scanner **we need to make this read in scf
 	 */
-	public void tokenize() {}
-	
-	
+	public void tokenize(final Reader input) {
+		tokens = new LinkedList<Token>();
+		GeneratedScanner scanner = new GeneratedScanner (input);
+		try {
+            Token token = scanner.yylex();
+            while (token != null && token.getTokenType() != TokenKinds.EOF) {
+                tokens.add (token);
+                token = scanner.yylex();
+            }
+        } catch (IOException ex) {
+            // Not necessarily a problem, depending on the input source
+        }
+	}
 	
 	/**
 	 * Compares 2 files for equality
