23e3c6dceb76afdd1d118bf083620064266462c6
Jacinda Rosario <jrosa005@odu.edu>
2021-11-14 21:30:40 -0500

    test tokenize

--- src/test/java/edu/odu/cs/cs350/TestSourceCodeFile.java
+++ src/test/java/edu/odu/cs/cs350/TestSourceCodeFile.java
@@ -1,5 +1,6 @@
 package edu.odu.cs.cs350;
 
+import java.io.Reader;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -8,16 +9,21 @@ class TestSourceCodeFile {
 
 	@Test
 	void testSourceCodeFile() {
-		SourceCodeFile s = new SourceCodeFile("/home/cs_vdulc001/cppTest.cpp");
-		assertTrue(s.isCppFile());
+		//SourceCodeFile s = new SourceCodeFile("/home/cs_vdulc001/cppTest.cpp");
+		//assertTrue(s.isCppFile());
+		SourceCodeFile s = new SourceCodeFile("src/test/resources/test1.cpp");
 		assertTrue(s.getFile().exists());
-		assertEquals(s.getPath(), "/home/cs_vdulc001/cppTest.cpp");
-		assertEquals(s.tokenize(), 0);
+		assertEquals(s.getPath(), "src/test/resources/test1.cpp");
+		Reader input = s.getInput(s.getPath());
+	    s.tokenize(input);
+	    assertEquals(s.calculateTotalTokens(), 17);
 		
-		SourceCodeFile r = new SourceCodeFile("/home/cs_vdulc001/cppTest.cpp");
+		
+		//SourceCodeFile r = new SourceCodeFile("/home/cs_vdulc001/cppTest.cpp");
+	    SourceCodeFile r = new SourceCodeFile("src/test/resources/test1.cpp");
 		assertTrue(s.equals(r));
 		assertNotNull(s.toString());
-		
+	
 	}
 
 }
