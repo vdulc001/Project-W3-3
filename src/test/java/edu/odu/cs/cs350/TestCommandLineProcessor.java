f508284490fd648debf90f2c6580b3ae14e57414
Sheldon Deans <sdean008@odu.edu>
2021-11-17 03:15:13 -0500

    Removed header comment

--- src/test/java/edu/odu/cs/cs350/TestCommandLineProcessor.java
+++ src/test/java/edu/odu/cs/cs350/TestCommandLineProcessor.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package edu.odu.cs.cs350;
 
 import static edu.odu.cs.cs350.CommandLineProcessor.findFilesInNestedDirectories;
