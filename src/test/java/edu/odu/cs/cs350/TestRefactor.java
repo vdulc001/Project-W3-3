03ed92a713d767307b1ffac7a0506622659163b6
carlojdiaz <carlodiaz139@gmail.com>
2021-11-28 15:50:00 -0500

    Update test class to focus TDD

--- src/test/java/edu/odu/cs/cs350/TestRefactor.java
+++ src/test/java/edu/odu/cs/cs350/TestRefactor.java
@@ -18,7 +18,6 @@ public class TestRefactor {
 
     @Test
     void testMutators(){
-<<<<<<< HEAD
         SourceCodeFile scf1 = new SourceCodeFile("src/test/data/test1.cpp");
         SourceCodeFile scf2 = new SourceCodeFile("src/test/data/test1.h");
         Token tok1 = new Token(TokenKinds.INT, 10, 5);
@@ -44,33 +43,9 @@ public class TestRefactor {
         refactor.setNumRefactors(5);
         assertEquals(refactor.getNumRefactors(), 5);
 
-=======
-        SourceCodeFile scf1 = new SourceCodeFile("test1.cpp");
-        SourceCodeFile scf2 = new SourceCodeFile("test1.h");
-        ArrayList<SourceCodeFile> listOfFiles = new ArrayList<SourceCodeFile>();
-
-        listOfFiles.add(scf1);
-        Refactoring refactor = new Refactoring(0, listOfFiles);
-
-        assertNotNull(refactor.getFiles());
-        assertEquals(refactor.getFiles(), listOfFiles);
-
-        listOfFiles.add(scf2);
-        refactor.addFile(scf2);
-        assertNotNull(refactor.getFiles());
-        assertEquals(refactor.getFiles(), listOfFiles);
-
-
-        assertEquals(refactor.getnSuggestions(), 0);
-
-        refactor.setnSuggestions(5);
-        assertEquals(refactor.getnSuggestions(), 5);
->>>>>>> e117d9b3b9e7e48b6b813c869f6c144fc61c8bcd
     }
 
-    /*
     @Test
-<<<<<<< HEAD
     void testSingleRefactoringFound(){
         SourceCodeFile scf1 = new SourceCodeFile("src/test/data/test2.cpp");
         ArrayList<SourceCodeFile> listOfFiles = new ArrayList<SourceCodeFile>();
@@ -91,32 +66,7 @@ public class TestRefactor {
                         scf1.getPath() + ":" + tok2.getLine() + ":" + tok2.getColumn() + "\nb 5\n" +
                         scf1.getPath() + ":" + tok3.getLine() + ":" + tok3.getColumn() + "\nc 3\n" +
                         scf1.getPath() + ":" + tok4.getLine() + ":" + tok4.getColumn() + "\nd 8\n");
-=======
-    void testDuplicateCodefound() throws Exception{
-        ArrayList<SourceCodeFile> listOfFiles = new ArrayList<SourceCodeFile>();
-        SourceCodeFile scf = new SourceCodeFile("src/test/data/test1.cpp");
-        listOfFiles.add(scf);
-        Refactoring refactor = new Refactoring(0, listOfFiles);
-
-        assertEquals(refactor.getNumRefactors(), 0);
-
-        refactor.findRefactored();
-        assertNotEquals(refactor.getNumRefactors(), 0);
-
-        LinkedList<Token> duplicateCode = new LinkedList<Token>();
-        Token tok1 = new Token(TokenKinds.INT, 11, 5);
-        Token tok2 = new Token(TokenKinds.IDENTIFIER, 11, 9);
-        Token tok3 = new Token(TokenKinds.OPERATOR, 11, 11);
-        Token tok4 = new Token(TokenKinds.INTEGER_LITERAL, 11, 13);
-        duplicateCode.add(tok1);
-        duplicateCode.add(tok2);
-        duplicateCode.add(tok3);
-        duplicateCode.add(tok4);
-
-        assertEquals(refactor.getRefactoredOpportunities(), duplicateCode);
->>>>>>> e117d9b3b9e7e48b6b813c869f6c144fc61c8bcd
     }
-    */
     
     @Test
     void testMultipleRefactoringsFound(){
