ff1aae8b9e35fe5e0ef83d3dcb6b36dd9c2ad872
Jacinda Rosario <jrosa005@odu.edu>
2021-11-30 19:47:15 -0500

    revise tests to go along with new test data cpp file

--- src/test/java/edu/odu/cs/cs350/TestTokenScanner.java
+++ src/test/java/edu/odu/cs/cs350/TestTokenScanner.java
@@ -1,6 +1,5 @@
 package edu.odu.cs.cs350;
 
-import java.io.Reader;
 import java.util.LinkedList;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -16,28 +15,37 @@ public class TestTokenScanner {
 	@Test
     public final void testScanner() throws Exception{
 		SourceCodeFile s = new SourceCodeFile("src/test/data/test1.cpp");
-		Reader input = s.getInput("src/test/data/test1.cpp");
-        LinkedList<Token> tokens = new LinkedList<Token>();
-        s.tokenize(/*input, tokens*/);
-        assertEquals (21, tokens.size());
+        LinkedList<Token> tokens = s.getTokens();
+        LinkedList<Token> testTokens = new LinkedList<Token>();
+        for (Token tok: tokens) {
+        	testTokens.add(tok);
+        }
         
-        Token t = tokens.get(4);
+        assertEquals (72, s.calculateTotalTokens());
+        
+        Token t = tokens.get(1);
+        assertEquals (TokenKinds.PREPROCESSOR, t.getTokenType());
+        assertEquals ("#include", t.getLexeme());
+        assertEquals (1, t.getLine());
+        assertEquals (1, t.getColumn());
+        
+        t = tokens.get(5);
         assertEquals (TokenKinds.USING, t.getTokenType());
-        //assertEquals ("using", t.getLexeme());
+        assertEquals ("using", t.getLexeme());
         assertEquals (3, t.getLine());
         assertEquals (1, t.getColumn());
         
-        t = tokens.get(8);
+        t = tokens.get(9);
         assertEquals (TokenKinds.INT, t.getTokenType());
-        //assertEquals ("int", t.getLexeme()); //fix getLexeme();
+        assertEquals ("int", t.getLexeme()); //fix getLexeme();
         assertEquals (7, t.getLine());
         assertEquals (1, t.getColumn());
         
-        t = tokens.get(1);
-        assertEquals (TokenKinds.LT, t.getTokenType());
-        //assertEquals ("<", t.getLexeme());
-        assertEquals (1, t.getLine());
-        assertEquals (10, t.getColumn());
+        t = tokens.get(72);
+        assertEquals (TokenKinds.RBRACE, t.getTokenType());
+        assertEquals ("}", t.getLexeme());
+        assertEquals (26, t.getLine());
+        assertEquals (1, t.getColumn()); 
        
 	}
 
