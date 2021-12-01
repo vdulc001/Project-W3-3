da660dd3cec0caaa69c40f668f810c7c9ebe5084
carlojdiaz <carlodiaz139@gmail.com>
2021-12-01 16:51:39 -0500

    Update test files

--- src/test/data/test1.cpp
+++ src/test/data/test1.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 
-using namespace std;
-
 //Test cpp file
 
 int main(){
-    cout << "Hello World!";
 
     //Next 4 lines of code are duplicates, first refactoring
     int a = 4;
