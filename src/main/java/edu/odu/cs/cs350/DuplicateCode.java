67c3923fd80c94935196fbf22ccd3ff5e9751ddd
cjackal9 <91925633+cjackal9@users.noreply.github.com>
2021-12-05 23:48:41 -0500

    Implementing sourced Suffix Tree

--- src/main/java/edu/odu/cs/cs350/DuplicateCode.java
+++ src/main/java/edu/odu/cs/cs350/DuplicateCode.java
@@ -18,15 +18,39 @@ import java.io.*;
 public class DuplicateCode{
 
 File currentFile;
+ArrayList<Token> Test
 
 public void SetCurrentfile()
 {
 	SourceCodeFile.getFile();
 }
 
+
+
+public void SetArray()
+{
+  Test = SourceCodeFile.getTokens();
+}
+
 public DetectSimilarTokens() {
   SharedPhrases phrases = new SharedPhrases();
-  SourceCodeFile.
+ String filename =  file(currentfile).getname;
+  //Loop through List//
+ for(i = 0; i < Test.size(); i++)
+ {
+	 Token = Test.(i);
+	//Build Suffix Tree//
+	phrases.addSentence(Token, filename); 
+ }
+ for (Token p: Tokens.Test()) {
+	   System.out.print (p + ":");
+	   for (String source: tokens.sourcesOf(p)) {
+	       System.out.print (" " + source);
+	   } 
+	   System.out.println();
+	}
+
+  
 }
 
 
